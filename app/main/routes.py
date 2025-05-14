a982e5aab415c074b1de0596596616e79978fb92
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2025-05-14 02:14:26 -0700

    routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user
 from app.extensions import db
-from app.models import Recipe, Category, User
+from app.models import Recipe, Category, User, Ingredient, RecipeIngredient, Comment
+from app.main.forms import RecipeForm, CommentForm
 
 main = Blueprint('main', __name__)
 
@@ -64,21 +65,61 @@ def all_recipes():
                           selected_category=category_id,
                           title='All Recipes')
 
-@main.route('/recipes/<int:recipe_id>')
+@main.route('/recipes/<int:recipe_id>', methods=['GET', 'POST'])
 def recipe_detail(recipe_id):
-    """Display a specific recipe with details."""
+    """Display a specific recipe with details and handle comments."""
     recipe = Recipe.query.get_or_404(recipe_id)
+    form = CommentForm()
+    
+    if form.validate_on_submit() and current_user.is_authenticated:
+        comment = Comment(
+            content=form.content.data,
+            recipe_id=recipe.id,
+            user_id=current_user.id
+        )
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been added!', 'success')
+        return redirect(url_for('main.recipe_detail', recipe_id=recipe.id))
+        
     return render_template('main/recipe_detail.html', 
                            recipe=recipe,
+                           form=form,
                            title=recipe.title)
 
 @main.route('/recipes/new', methods=['GET', 'POST'])
 @login_required
 def new_recipe():
     """Create a new recipe."""
-    # This will be implemented later with forms
-    flash('Recipe creation form will be available soon!', 'info')
-    return redirect(url_for('main.all_recipes'))
+    form = RecipeForm()
+    
+    if form.validate_on_submit():
+        recipe = Recipe(
+            title=form.title.data,
+            description=form.description.data,
+            preparation_time=form.preparation_time.data,
+            cooking_time=form.cooking_time.data,
+            servings=form.servings.data,
+            image_url=form.image_url.data,
+            user_id=current_user.id
+        )
+        
+        # Add categories
+        for category_id in form.category_ids.data:
+            category = Category.query.get(category_id)
+            if category:
+                recipe.categories.append(category)
+        
+        db.session.add(recipe)
+        db.session.commit()
+        
+        flash('Your recipe has been created!', 'success')
+        return redirect(url_for('main.recipe_detail', recipe_id=recipe.id))
+    
+    return render_template('main/recipe_form.html', 
+                           form=form,
+                           title='Create Recipe',
+                           heading='Create a New Recipe')
 
 @main.route('/recipes/<int:recipe_id>/edit', methods=['GET', 'POST'])
 @login_required
@@ -91,9 +132,43 @@ def edit_recipe(recipe_id):
         flash('You can only edit your own recipes!', 'danger')
         return redirect(url_for('main.recipe_detail', recipe_id=recipe.id))
     
-    # This will be implemented later with forms
-    flash('Recipe editing form will be available soon!', 'info')
-    return redirect(url_for('main.recipe_detail', recipe_id=recipe.id))
+    form = RecipeForm()
+    
+    if request.method == 'GET':
+        # Pre-fill the form with existing recipe data
+        form.title.data = recipe.title
+        form.description.data = recipe.description
+        form.preparation_time.data = recipe.preparation_time
+        form.cooking_time.data = recipe.cooking_time
+        form.servings.data = recipe.servings
+        form.image_url.data = recipe.image_url
+        form.category_ids.data = [category.id for category in recipe.categories]
+    
+    if form.validate_on_submit():
+        # Update recipe with form data
+        recipe.title = form.title.data
+        recipe.description = form.description.data
+        recipe.preparation_time = form.preparation_time.data
+        recipe.cooking_time = form.cooking_time.data
+        recipe.servings = form.servings.data
+        recipe.image_url = form.image_url.data
+        
+        # Update categories
+        recipe.categories = []
+        for category_id in form.category_ids.data:
+            category = Category.query.get(category_id)
+            if category:
+                recipe.categories.append(category)
+        
+        db.session.commit()
+        
+        flash('Your recipe has been updated!', 'success')
+        return redirect(url_for('main.recipe_detail', recipe_id=recipe.id))
+    
+    return render_template('main/recipe_form.html', 
+                           form=form,
+                           title='Edit Recipe',
+                           heading=f'Edit Recipe: {recipe.title}')
 
 @main.route('/recipes/<int:recipe_id>/delete', methods=['POST'])
 @login_required
@@ -111,3 +186,22 @@ def delete_recipe(recipe_id):
     
     flash('Your recipe has been deleted!', 'success')
     return redirect(url_for('main.all_recipes'))
+
+@main.route('/comments/<int:comment_id>/delete', methods=['POST'])
+@login_required
+def delete_comment(comment_id):
+    """Delete a comment."""
+    comment = Comment.query.get_or_404(comment_id)
+    
+    # Check if the current user is the owner of the comment
+    if comment.user_id != current_user.id:
+        flash('You can only delete your own comments!', 'danger')
+        return redirect(url_for('main.recipe_detail', recipe_id=comment.recipe_id))
+    
+    recipe_id = comment.recipe_id
+    
+    db.session.delete(comment)
+    db.session.commit()
+    
+    flash('Your comment has been deleted!', 'success')
+    return redirect(url_for('main.recipe_detail', recipe_id=recipe_id))
